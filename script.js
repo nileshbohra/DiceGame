ce42b81faf9cd1424958a88c51533737d844800e
Nike2000 <nmbohra2001@gmail.com>
2020-12-12 22:22:46 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let img1 = "./images/dice1.png";
+let img2 = "./images/dice2.png";
+let img3 = "./images/dice3.png";
+let img4 = "./images/dice4.png";
+let img5 = "./images/dice5.png";
+let img6 = "./images/dice6.png";
+
+let arImg = [img1, img2, img3, img4, img5, img6];
+// console.log(arImg);
+let ran1 = Math.floor(Math.random() * arImg.length);
+// console.log(ran1);
+let ran2 = Math.floor(Math.random() * arImg.length);
+// console.log(ran2);
+
+document.querySelector(".img1").setAttribute('src', arImg[ran1]);
+document.querySelector(".img2").setAttribute('src', arImg[ran2]);
+
+if (ran1 > ran2) {
+    document.querySelector(".container>h1").textContent = "Player 1 Won";
+}
+else if (ran1===ran2) {
+    document.querySelector(".container>h1").textContent = "Match Tied";
+} else {
+    document.querySelector(".container>h1").textContent = "Player 2 Won";
+}
